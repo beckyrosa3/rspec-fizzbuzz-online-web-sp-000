b453113f96550b5a186d7ebd9aaf183f0e5a7e1f
Becky Rosa <becky_m_rosa@yahoo.com>
2020-12-30 18:05:23 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(num)
 (1..100).each do |num|
- if num % 3 == 0
-   "Fizz"
+elsif (num % 3 == 0) && (num % 5 == 0)
+  "FizzBuzz"
+elsif num % 3 == 0
+     "Fizz"
  elsif num % 5 == 0
    "Buzz"
- elsif num % 3 == 0 && num % 5 == 0
-   "FizzBuzz"
  else
    num = num
  end
