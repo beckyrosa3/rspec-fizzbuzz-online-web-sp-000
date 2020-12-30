980aadf5107934154b1154f317fe68463fb20a83
Becky Rosa <becky_m_rosa@yahoo.com>
2020-12-30 16:10:41 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz
 (1..100).each do |num|
  if num % 3 == 0
-   num = "Fizz"
+   puts = "Fizz"
  elsif num % 5 == 0
-   num = "Buzz"
+   puts = "Buzz"
  elsif num % 3 == 0 && num % 5 == 0
-   num = "FizzBuzz"
+   puts = "FizzBuzz"
  else
    num = num
  end
