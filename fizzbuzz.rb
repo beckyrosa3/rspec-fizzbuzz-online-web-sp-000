716ac1ca6a56bfab5dd98a6406edabbfb1514796
Becky Rosa <becky_m_rosa@yahoo.com>
2020-12-30 16:08:26 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz
+(1..100).each do |num|
+ if num % 3 == 0
+   num = "Fizz"
+ elsif num % 5 == 0
+   num = "Buzz"
+ elsif num % 3 == 0 && num % 5 == 0
+   num = "FizzBuzz"
+ else
+   num = num
+ end
+end
+end
